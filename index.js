880df5bf4be405f8a48c384716e26821a3b0a3ac
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-08-16 19:38:58 -0600

    Saving

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var pg = require('pg');
+var querystring = require('querystring');
 
 var db = new pg.Client(process.env.DATABASE_URL);
 db.connect(function(err) {
@@ -11,17 +12,173 @@ db.connect(function(err) {
 
 var PORT = process.env.PORT || 5555;
 
+var ALLOWED_CHARS = charsFrom('a', 'z')+charsFrom('A', 'Z')+charsFrom('0', '9')+"-_.";
+
 var die = function(res, err, data, type) {
 	if(!type) type = "text/plain";
 	res.writeHead(err, {"Content-type": type});
-	res.write(data);
+	res.write(data+"");
 	res.end();
+};
+
+var mergeInto = function(target, source) {
+	for(var k in source) {
+		target[k] = source[k];
+	}
+};
+
+var getFields = function(req, callback) {
+	var url = req.url;
+	var params = {};
+	var ind = url.indexOf("?");
+	if(ind > -1) {
+		mergeInto(params, querystring.parse(url.substring(ind+1)));
+		url = url.substring(0, ind);
+	}
+	if(req.method === "POST") {
+		var body = '';
+		req.on('data', function(data) {
+			body += data;
+			if(body.length > 1e6) {
+				req.connection.destroy();
+			}
+		});
+		req.on('end', function() {
+			mergeInto(params, querystring.parse(body));
+			callback(null, params, url);
+		});
+	}
+	else {
+		callback(null, params, url);
+	}
+};
+
+function charsFrom(start, end) {
+	var tr = "";
+	var startIndex = start.charCodeAt(0);
+	var endIndex = end.charCodeAt(0);
+	if(endIndex < startIndex) {
+		console.warn("charsFrom doesn't work like that");
+		return tr;
+	}
+	for(var i = startIndex; i < endIndex+1; i++) {
+		tr += String.fromCharCode(i);
+	}
+	return tr;
+}
+
+var attemptCreate = function(url, id, random, callback) {
+	if(arguments.length === 3) {
+		callback = random;
+		attemptCreate(url, id, false, callback);
+		return;
+	}
+	for(var i = 0; i < id.length; i++) {
+		if(ALLOWED_CHARS.indexOf(id[i]) === -1) {
+			callback("'"+id[i]+"' is not an allowed character");
+			return;
+		}
+	}
+	db.query("INSERT INTO links (url, id, random) VALUES ($1, $2, $3)", [url, id, !!random], function(err, result) {
+		if(err) {
+			if(err.code === "23505") {
+				callback("Already taken");
+				return;
+			}
+			console.log(err);
+			callback(err);
+		}
+		else {
+			callback(null, id);
+		}
+	});
+};
+
+var randomString = function(length) {
+	var tr = "";
+	for(var i = 0; i < length; i++) {
+		tr += ALLOWED_CHARS[Math.floor(Math.random()*ALLOWED_CHARS.length)];
+	}
+	return tr;
+};
+
+var attemptCreateRandom = function(url, length, triesLeft, callback) {
+	if(arguments.length === 2) {
+		callback = length;
+		db.query("SELECT * FROM links WHERE url=$1 AND random=true", [url], function(err, result) {
+			if(err) {
+				die(res, 500, err);
+				return;
+			}
+			if(result.rows.length === 0) {
+				attemptCreateRandom(url, 1, callback);
+			}
+			else {
+				callback(null, result.rows[0].id);
+			}
+		});
+		return;
+	}
+	if(arguments.length === 3) {
+		callback = triesLeft;
+		attemptCreateRandom(url, length, Math.pow(ALLOWED_CHARS.length, length-1), callback);
+		return;
+	}
+	var str = randomString(length);
+	attemptCreate(url, str, true, function(err, result) {
+		if(err) {
+			if(err != "Already taken") {
+				callback(err);
+				return;
+			}
+			// try again
+			if(triesLeft > 1) {
+				attemptCreateRandom(url, length, triesLeft-1, callback);
+			}
+			else {
+				attemptCreateRandom(url, length+1, callback);
+			}
+			return;
+		}
+		callback(result);
+	});
 };
 
 http.createServer(function(req, res) {
 	console.log(req.url);
 	if(req.url === "/") {
 		die(res, 200, "hi");
+	}
+	else if(req.url.indexOf('/api') === 0) {
+		getFields(req, function(err, fields, url) {
+			if(url === "/api/create") {
+				if(!("url" in fields)) {
+					die(res, 400, "URL missing");
+					return;
+				}
+				if(fields.custom) {
+					attemptCreate(fields.url, fields.custom, function(err, result) {
+						if(err) {
+							die(res, 400, err);
+							return;
+						}
+						die(res, 200, result);
+					});
+				}
+				else {
+					attemptCreateRandom(fields.url, function(err, result) {
+						if(err) {
+							die(res, 500, err);
+							return;
+						}
+						die(res, 200, result);
+					});
+				}
+			}
+			else {
+				die(res, 404, "API call not found.");
+			}
+		});
 	}
 	else {
 		var id = req.url.substring(1);
---
+++ migrations/20160817012915-RandomFlag.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+  db.addColumn("links", "random", {
+    type: "boolean",
+    notNull: true,
+    defaultValue: false
+  }, callback);
+};
+
+exports.down = function(db, callback) {
+  db.removeColumn("links", "random", callback);
+};
