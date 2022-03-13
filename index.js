3af2e4619ac3201ee70303bfa8cad6e831ad8b72
Colin Reeder <colin@vpzom.click>
2022-03-13 16:27:12 -0600

    Revert "bump random length again"
    This reverts commit 6e36eb2046cb0a729eb68417e844a797ea873e8b.

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ var attemptCreateRandom = function(domain, url, length, triesLeft, callback) {
 				return;
 			}
 			if(result.rows.length === 0) {
-				attemptCreateRandom(domain, url, 5, callback);
+				attemptCreateRandom(domain, url, 4, callback);
 			}
 			else {
 				callback(null, result.rows[0].id);
